e5639a21dcf0b3ac2116f7ec7d5debe3c2ce65f6
407332 <trecko@live.nl>
2017-10-30 11:25:43 +0100

    yo

--- src/TicketSeller.java
+++ src/TicketSeller.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 public class TicketSeller extends AbstractActor {
 
     private ActorRef customer;
-    private ActorRef seatManager;
     private ArrayList<ActorRef> seatManagers = new ArrayList<>();
 
 
@@ -16,10 +15,10 @@ public class TicketSeller extends AbstractActor {
         return receiveBuilder()
                 .match(Reserve.class, msg -> {
                     int section = msg.getSection();
-
                     customer = getSender();
                     int numberofseats = msg.getNumberofseats();
                     IsAvailable message = new IsAvailable(numberofseats);
+                    ActorRef seatManager = seatManagers.get(section-1);
                     seatManager.tell(message, getSelf());
                 })
                 .match(Buy.class, msg -> {
@@ -41,7 +40,7 @@ public class TicketSeller extends AbstractActor {
     }
 
     private void generateSeatmanagers(){
-        for (int i=1; i <= 7; i++){
+        for (int i=0; i <= 6; i++){
             ActorRef temp = getContext().actorOf(SeatManager.prop(i, 40, getSelf()), "SM"+ i);
             seatManagers.add(temp);
             System.out.println("Seatmanager "+ i + "created");
